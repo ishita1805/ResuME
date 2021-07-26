53707ee440302ab138414a41916e1256bcd24c88
Ishita Kabra <ishitakabra1805@gmail.com>
2021-07-26 18:04:18 +0530

    template

--- CLI/main.py
+++ CLI/main.py
@@ -3,21 +3,19 @@ from commands import init, build, websites, welcome
 from utils import getENV
 
 def initfunc():
-    welcome()
     init()
 
 def buildfunc():
     if(getENV("PAT")==None or getENV("username")==None or getENV("password")==None):
         print("please use 'init' command first");
         return;
-    welcome()
     build()
 
 def listfunc():
-    welcome()
     websites()
 
 if __name__ == '__main__':
+    welcome()
     fire.Fire({
         'init': initfunc,
         'build': buildfunc,
--- node_wrapper/server.js
+++ node_wrapper/server.js
@@ -2,68 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const axios = require('axios');
 const PORT  =  process.env.PORT || 3001
+const deployRoutes = require('./src/routes/deploy')
 
 app.use(express.json());
 app.use(cors())
 
+app.use('/deploy',deployRoutes)
+
 
 app.get('/', async(req,res) => {
    res.send('API Health OK!');
-})
-
-
-app.get('/auth', async(req,res) => {
-    axios.get('https://www.linkedin.com/oauth/v2/authorization',{
-        params:{
-            response_type:"code",
-            client_id:process.env.ID,
-            redirect_uri:'http://localhost:3001/red',
-            state:"ybwxpbktwuhcewp0b543238sd",
-            scope:'r_liteprofile r_emailaddress'
-        }
-    })
-    .then((resp)=>res.redirect(resp.request.res.responseUrl))
-    .catch((er)=>res.json(er));
-})
-
-app.get('/red', async(req,res) => {
-    axios.post('https://www.linkedin.com/oauth/v2/accessToken',null,{
-        params:{
-            grant_type:'authorization_code',
-            client_id: process.env.ID,
-            client_secret: process.env.SECRET,
-            redirect_uri:'http://localhost:3001/red',
-            code: req.query.code,
-        },
-        headers: {'Content-Type': 'x-www-form-urlencoded'}
-    })
-    .then((resp)=>{
-        // console.log(resp);
-        // res.send(resp.data);
-        axios.get('https://api.linkedin.com/v2/me',{
-            params:{
-                oauth2_access_token: resp.data.access_token,
-            }
-        })
-        .then((resp) => {
-            console.log(resp);
-            res.json({
-                data: resp.data
-            })
-        })
-        .catch((e) => {
-            console.log(e);
-            res.json({
-                e
-            })
-        })
-    })
-    .catch((e)=>{
-        console.log(e);
-        res.json(e);
-    })
 })
 
 // github openapi endpoints: https://api.github.com/
